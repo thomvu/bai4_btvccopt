9085dcabacb3b3895a366e1ba73ff12df9e4539c
thomvt <cayla9x@gmail.com>
2018-07-19 09:45:05 +0700

    initial commit

---
+++ src/main/java/com/mycompany/bai4_bt2/Bai4.java
@@ -0,0 +1,66 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.bai4_bt2;
+
+import com.google.common.cache.CacheBuilder;
+import com.google.common.cache.CacheLoader;
+import com.google.common.cache.LoadingCache;
+import java.util.concurrent.TimeUnit;
+import static sun.security.x509.CertificateVersion.V1;
+
+/**
+ *
+ * @author Thom
+ */
+public class Bai4 {
+
+    private static LoadingCache<Integer, String> cache;
+
+    static {
+        cache = CacheBuilder.newBuilder()
+                .maximumSize(100) //set size
+                .expireAfterWrite(20, TimeUnit.SECONDS) //set time expire
+                .expireAfterAccess(10, TimeUnit.SECONDS)
+                .build(new CacheLoader<Integer, String>() {
+                    @Override
+                    public String load(Integer k) throws Exception {
+                        return getPrime(k);
+                        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+                    }
+
+                });
+    }
+
+    public static LoadingCache<Integer, String> getLoadingCache() {
+        return cache;
+    }
+
+    private static boolean checkPrimeNumber(int n) {
+        if (n < 2) {
+            return false;
+        }
+        if (n == 2) {
+            return true;
+        }
+        int m = (int) Math.sqrt(n);
+        for (int i = 2; i <= m; i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+    private static String getPrime(Integer n) {
+        StringBuilder result = new StringBuilder();
+        for (int i = 2; i <= n; i++) {
+            if (checkPrimeNumber(i) == true) {
+                result.append(i).append(",  ");
+            }
+        }
+        String result1 = result.substring(0, result.length() - 3);
+        return result1.toString();
+    }
+}
---
+++ src/main/java/com/mycompany/bai4_bt2/GuavaTest.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.bai4_bt2;
+
+import com.google.common.cache.LoadingCache;
+import java.util.concurrent.ExecutionException;
+import static spark.Spark.get;
+
+/**
+ *
+ * @author Thom
+ */
+public class GuavaTest {
+    
+    public static void main(String[] args) {
+        GuavaTest guavaTest = new GuavaTest();
+        get("prime", "application/json", (request, response) -> {
+            String id = request.queryParams("n");
+            return guavaTest.getStudentUsingGuava(Integer.parseInt(id));
+        });
+        
+//        try {
+//            // Access student first time with id 1, getStudentUsingGuava() will
+//            // be called.
+//            System.out.println(guavaTest.getStudentUsingGuava(100));
+//            System.out.println("------------------------");
+//
+//            // The second time we get student, data will cache
+//            System.out.println(guavaTest.getStudentUsingGuava(100));
+//        } catch (ExecutionException e) {
+//        }
+    }
+
+    private String getStudentUsingGuava(int id) throws ExecutionException {
+        LoadingCache<Integer, String> cache = Bai4.getLoadingCache();
+        System.out.println("Cache Size:" + cache.size());
+        return cache.get(id);
+    }
+
+}
